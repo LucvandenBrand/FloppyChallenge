cfab3aaf01b1a6c605ee457a2e8ad43b58d282d3
Luc van den Brand <Lucvandenbrand@protonmail.com>
2019-08-01 22:36:53 +0200

    Removed trash from template.

--- floppies/template/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy
-# Build directory: /home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("source")
-subdirs("tests")
--- floppies/template/cmake-build-debug/tests/CMakeFiles/valgrind_test_thingy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/valgrind_test_thingy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/valgrind_test_thingy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
