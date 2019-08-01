c43e743dfe7c46bebe7caf94086315b6e0abcf5c
Luc van den Brand <Lucvandenbrand@protonmail.com>
2019-08-01 22:36:17 +0200

    Removed trash.

--- floppies/chip-8-emu/cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/tests
-# Build directory: /home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_thingy "/home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/cmake-build-debug/tests/test_thingy")
-set_tests_properties(test_thingy PROPERTIES  TIMEOUT "30" WORKING_DIRECTORY "/home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/cmake-build-debug/Testing" _BACKTRACE_TRIPLES "/home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/tests/CMakeLists.txt;24;add_test;/home/luc/Documents/Work/Side_Projects/Floppy_Challenge/Repo/floppies/thingy/tests/CMakeLists.txt;0;")
