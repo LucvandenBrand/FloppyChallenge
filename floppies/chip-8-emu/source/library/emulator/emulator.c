f610eadb7905a05ec1e457fb14fcc7793f5f7d68
Luc van den Brand <Lucvandenbrand@protonmail.com>
2019-08-26 22:19:08 +0200

    Created implementation of copy video memory.

--- floppies/chip-8-emu/source/library/emulator/emulator.c
+++ floppies/chip-8-emu/source/library/emulator/emulator.c
@@ -15,10 +15,10 @@ void emulate_rom(const BinaryBlob * rom)
         step_system_cpu(&system);
         if (system.video_changed)
         {
-            //copy_system_video_memory(system.video_memory, frame_buffer);
-            //present_frame_buffer(render_context, frame_buffer);
+            copy_system_video_memory(system, frame_buffer);
+            present_frame_buffer(render_context, frame_buffer);
         }
-        //update_system_key_states(system.key_states);
+        //update_system_key_states(system);
     }
     free_frame_buffer(&frame_buffer);
     free_render_context(&render_context);
@@ -63,4 +63,16 @@ void step_timers(System * system)
     if (system->sound_timer == 1)
         system->audio_triggered = true;
     --system->sound_timer;
+}
+
+void copy_system_video_memory(System system, FrameBuffer frame_buffer)
+{
+    for (int hor_index = 0; hor_index < VIDEO_WIDTH; hor_index++)
+    {
+        for (int ver_index = 0; ver_index < VIDEO_HEIGHT; ver_index++)
+        {
+            uint8_t pixel = system.video_memory[ver_index * VIDEO_WIDTH + hor_index];
+            frame_buffer.pixels[ver_index * frame_buffer.frame_width + hor_index] = (uint32_t) pixel;
+        }
+    }
 }
--- floppies/chip-8-emu/source/library/includes/emulator/emulator.h
+++ floppies/chip-8-emu/source/library/includes/emulator/emulator.h
@@ -2,10 +2,12 @@
 #define EMULATOR_H
 
 #include <emulator/system.h>
+#include <rendering/frame_buffer.h>
 
 void emulate_rom(const BinaryBlob * rom);
 void step_system_cpu(System * system);
 void process_op_code(System * system, uint16_t op_code);
 void step_timers(System * system);
+void copy_system_video_memory(System system, FrameBuffer frame_buffer);
 
 #endif
