9d0ecab28f82644bab925fafb94984c9b3f9840b
Luc van den Brand <lucvandenbrand@protonmail.com>
2019-10-24 13:46:44 +0200

    Added more tests to parser.

--- floppies/text-adventure/tests/suites/parser_suite.c
+++ floppies/text-adventure/tests/suites/parser_suite.c
@@ -13,19 +13,57 @@ START_TEST(test_empty_text_to_tokens)
 END_TEST
 
 START_TEST(test_look_at_to_tokens)
-    {
-        GameState game = init_game_state(GAME_DATA_PATH);
-        TokenList token_list = text_to_tokens("look at Game Console", game);
-        ck_assert_int_eq(token_list.tokens[0].type, LOOK);
-        ck_assert_int_eq(token_list.tokens[1].type, AT);
-        ck_assert_int_eq(token_list.tokens[2].type, ITEM);
-        ck_assert_int_eq(token_list.tokens[2].value, 0);
-        ck_assert_int_eq(token_list.length, 3);
-        free_tokens(&token_list);
-        free_game_state(&game);
-    }
+{
+    GameState game = init_game_state(GAME_DATA_PATH);
+    TokenList token_list = text_to_tokens("look at Game Console", game);
+    ck_assert_int_eq(token_list.tokens[0].type, LOOK);
+    ck_assert_int_eq(token_list.tokens[1].type, AT);
+    ck_assert_int_eq(token_list.tokens[2].type, ITEM);
+    ck_assert_int_eq(token_list.tokens[2].value, 0);
+    ck_assert_int_eq(token_list.length, 3);
+    free_tokens(&token_list);
+    free_game_state(&game);
+}
 END_TEST
 
+START_TEST(test_take_item_to_tokens)
+{
+    GameState game = init_game_state(GAME_DATA_PATH);
+    TokenList token_list = text_to_tokens("take Game Console", game);
+    ck_assert_int_eq(token_list.tokens[0].type, TAKE);
+    ck_assert_int_eq(token_list.tokens[1].type, ITEM);
+    ck_assert_int_eq(token_list.tokens[1].value, 0);
+    ck_assert_int_eq(token_list.length, 2);
+    free_tokens(&token_list);
+    free_game_state(&game);
+}
+END_TEST
+
+START_TEST(test_place_item_to_tokens)
+{
+    GameState game = init_game_state(GAME_DATA_PATH);
+    TokenList token_list = text_to_tokens("place Game Console", game);
+    ck_assert_int_eq(token_list.tokens[0].type, PLACE);
+    ck_assert_int_eq(token_list.tokens[1].type, ITEM);
+    ck_assert_int_eq(token_list.tokens[1].value, 0);
+    ck_assert_int_eq(token_list.length, 2);
+    free_tokens(&token_list);
+    free_game_state(&game);
+}
+END_TEST
+
+START_TEST(test_walk_direction_to_tokens)
+{
+    GameState game = init_game_state(GAME_DATA_PATH);
+    TokenList token_list = text_to_tokens("walk north", game);
+    ck_assert_int_eq(token_list.tokens[0].type, WALK);
+    ck_assert_int_eq(token_list.tokens[1].type, DIRECTION);
+    ck_assert_int_eq(token_list.tokens[1].value, NORTH);
+    ck_assert_int_eq(token_list.length, 2);
+    free_tokens(&token_list);
+    free_game_state(&game);
+}
+END_TEST
 
 Suite * makeParserSuite()
 {
@@ -34,6 +72,9 @@ Suite * makeParserSuite()
     TCase * test_case = tcase_create("Parser");
     tcase_add_test(test_case, test_empty_text_to_tokens);
     tcase_add_test(test_case, test_look_at_to_tokens);
+    tcase_add_test(test_case, test_take_item_to_tokens);
+    tcase_add_test(test_case, test_place_item_to_tokens);
+    tcase_add_test(test_case, test_walk_direction_to_tokens);
     suite_add_tcase(suite, test_case);
 
     return suite;
