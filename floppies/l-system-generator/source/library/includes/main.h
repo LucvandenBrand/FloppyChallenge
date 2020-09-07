3a1f0bbdd298ff30aeb9136207c354b3602a362b
Luc van den Brand <lucvandenbrand@protonmail.com>
2020-09-07 17:19:36 +0200

    Refactored main.

--- floppies/l-system-generator/source/library/includes/main.h
+++ floppies/l-system-generator/source/library/includes/main.h
@@ -3,6 +3,8 @@
 
 #include "job.h"
 
-int handle_job(Job job);
+bool handle_job(Job job);
+bool try_load_job(Job job, LSystem * system);
+bool try_save_job(Job job, LSystem system);
 
 #endif
--- floppies/l-system-generator/source/main.c
+++ floppies/l-system-generator/source/main.c
@@ -14,47 +14,61 @@ int main(int argc, char ** argv)
     return handle_job(job);
 }
 
-int handle_job(Job job)
+bool handle_job(Job job) {
+    LSystem system = alloc_empty_system();
+    if (!try_load_job(job, &system)) {
+        free_system(&system);
+        return false;
+    }
+
+    generate_system(&system, job.num_iterations);
+
+    if (!try_save_job(job, system))
+    {
+        free_system(&system);
+        return false;
+    }
+
+    free_system(&system);
+    return true;
+}
+
+bool try_load_job(Job job, LSystem * system)
 {
-    // Load the L-system into memory
     long input_size = get_file_size(job.input_path);
     if (input_size < 1)
     {
         printf("Could not read input file.\n");
-        return EXIT_FAILURE;
+        return false;
     }
     char * input_buffer = safe_malloc(sizeof(char) * input_size);
     try_load_file(job.input_path, input_buffer, input_size);
-
-    LSystem system = alloc_empty_system();
-    bool load_success = try_load_system_from_json_string(&system, input_buffer);
+    bool load_success = try_load_system_from_json_string(system, input_buffer);
     free(input_buffer);
     if (!load_success)
     {
         printf("Invalid input file.\n");
-        free_system(&system);
-        return EXIT_FAILURE;
+        return false;
     }
+    return true;
+}
 
-    // Load successful, start generating the system.
-    generate_system(&system, job.num_iterations);
+bool try_save_job(Job job, LSystem system)
+{
+    printf("Writing to output...\n");
 
     if (job.save_symbols)
     {
-        printf("Writing symbols to output...\n");
-        if (!try_write_file(job.output_path, (char *) system.axiom.symbols, system.axiom.length))
+        if (!try_write_file(job.output_path, system.axiom.symbols, system.axiom.length))
         {
             printf("Could not write to output file.\n");
-            free_system(&system);
-            return EXIT_FAILURE;
+            return false;
         }
-        printf("Success!\n");
     }
     else {
         // TODO: Render the system.
     }
 
-
-    free_system(&system);
-    return EXIT_SUCCESS;
+    printf("Success!\n");
+    return true;
 }
