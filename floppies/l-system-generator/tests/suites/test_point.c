68488088d5227cc3fad9ee57aacccf990ffd9dcf
Luc van den Brand <lucvandenbrand@protonmail.com>
2020-09-08 16:30:31 +0200

    Implemented tests for point methods.

--- floppies/l-system-generator/tests/suites/test_point.c
+++ floppies/l-system-generator/tests/suites/test_point.c
@@ -1,4 +1,5 @@
 #include <check.h>
+#include <math.h>
 #include "math/point.h"
 
 START_TEST(test_alloc_point_list)
@@ -36,6 +37,76 @@ START_TEST(test_add_point_to_list)
 }
 END_TEST
 
+START_TEST(test_normalize_point_list)
+{
+    PointList list = alloc_empty_point_list();
+    Point point_ll = {-10, -20};
+    add_point_to_list(point_ll, &list);
+    Point point_lr = {20, -21};
+    add_point_to_list(point_lr, &list);
+    Point point_ul = {-11, 30};
+    add_point_to_list(point_ul, &list);
+    Point point_ur = {10, 30};
+    add_point_to_list(point_ur, &list);
+    normalize_point_list(&list);
+    for (size_t index = 0; index < list.length; index++)
+    {
+        Point point = list.points[index];
+        ck_assert(point.x >= 0.0f);
+        ck_assert(point.x <= 1.0f);
+        ck_assert(point.y >= 0.0f);
+        ck_assert(point.y <= 1.0f);
+    }
+}
+END_TEST
+
+START_TEST(test_get_list_bounds)
+{
+    PointList list = alloc_empty_point_list();
+    Point low_point = {-10, 3};
+    add_point_to_list(low_point, &list);
+    Point mid_point = {5, 2};
+    add_point_to_list(mid_point, &list);
+    Point high_point = {23, 6};
+    add_point_to_list(high_point, &list);
+    Point min, max;
+    get_list_bounds(list, &min, &max);
+    ck_assert(min.x == low_point.x);
+    ck_assert(min.y == mid_point.y);
+    ck_assert(max.x == high_point.x);
+    ck_assert(max.y == high_point.y);
+    free_point_list(&list);
+}
+END_TEST
+
+START_TEST(test_point_length)
+{
+    Point point = {2, 4};
+    float ref_length = sqrtf(2*2 + 4*4);
+    float length = point_length(point);
+    ck_assert(length == ref_length);
+}
+END_TEST
+
+START_TEST(test_divide_point)
+{
+    Point point = {4, 7};
+    point = divide_point(point, 2);
+    ck_assert(point.x == 2.0f);
+    ck_assert(point.y == 3.5f);
+}
+END_TEST
+
+START_TEST(test_subtract_points)
+{
+    Point point = {4, 7};
+    Point subtractor = {1, 8};
+    point = subtract_points(point, subtractor);
+    ck_assert(point.x == 3.0f);
+    ck_assert(point.y == -1.0f);
+}
+END_TEST
+
 Suite * make_point_suite()
 {
     Suite * suite = suite_create("Point Test Suite");
@@ -43,6 +114,12 @@ Suite * make_point_suite()
     TCase * test_case = tcase_create("Test Cases");
     tcase_add_test(test_case, test_alloc_point_list);
     tcase_add_test(test_case, test_add_point_to_list);
+    tcase_add_test(test_case, test_normalize_point_list);
+    tcase_add_test(test_case, test_get_list_bounds);
+    tcase_add_test(test_case, test_point_length);
+    tcase_add_test(test_case, test_divide_point);
+    tcase_add_test(test_case, test_subtract_points);
+
     suite_add_tcase(suite, test_case);
 
     return suite;
